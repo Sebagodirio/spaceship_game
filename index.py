d70d71f6207edcd7d98d47d453ff26a9b4cc4dc0
SebaGodirio <sebastianlgodirio@gmail.com>
2020-08-26 19:18:45 -0300

    add enemy

--- index.py
+++ index.py
@@ -20,20 +20,75 @@ class spaceship(pygame.sprite.Sprite):
 
         self.velocity = 20
 
+    def leftMovement(self):
+        self.rect.left -=self.velocity
+        self.__movement()
+        
+    def rightMovement(self):
+        self.rect.right += self.velocity
+        self.__movement()
 
-
-    def shoot(self):
-        print("Disparo")
+    def shoot(self,x,y):
+        myProy = Proyectil(x,y)
+        self.shootList.append(myProy)
 
     def draw(self,superfice):
         superfice.blit(self.ImageSpaceship,self.rect)
 
-    def movement(self):
+    def __movement(self):
         if self.Life == True:
             if self.rect.left <= 0:
                 self.rect.left = 0
             elif self.rect.right > 870:
                 self.rect.right = 840
+
+class Proyectil(pygame.sprite.Sprite):
+    def __init__(self,posx,posy):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.imageProyectil = pygame.image.load("images/disparoa.jpg")
+        self.rect = self.imageProyectil.get_rect()
+        self.velocityShoot = 4
+
+        self.rect.top = posy
+        self.rect.left = posx
+
+    def trayectory(self):
+        self.rect.top = self.rect.top - self.velocityShoot
+
+    def draw(self,surface):
+        surface.blit(self.imageProyectil,self.rect)
+
+class invader(pygame.sprite.Sprite):
+    def __init__(self,posx,posy):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.imageA = pygame.image.load("images/MarcianoA.jpg")
+        self.imageB = pygame.image.load("images/MarcianoB.jpg")
+
+        self.imageList = [self.imageA,self.imageB]
+        self.posImage = 0
+
+        self.invaderImage = self.imageList[self.posImage]
+        self.rect = self.invaderImage.get_rect()
+        self.shootList = []
+        self.velocity = 20
+
+        self.rect.top = posy
+        self.rect.left = posx
+
+        self.timeChange = 1
+
+    def draw(self,surface):
+        self.invaderImage = self.imageList[self.posImage]
+        surface.blit(self.invaderImage,self.rect)
+    def behavior(self,time):
+        if self.timeChange == time:
+            self.posImage += 1 
+            self.timeChange += 1
+
+            if self.posImage > len(self.imageList):
+                self.posImage = 0
 
 def SpaceInvader():
     pygame.init()
@@ -41,12 +96,19 @@ def SpaceInvader():
     pygame.display.set_caption("Space Invader")
 
     background = pygame.image.load("images/fondo.jpg")
+
+    enemy = invader(100,100)
     player = spaceship()
 
+    demoProy = Proyectil(width/2,height-30)
     inGame = True
+
+    clock = pygame.time.Clock()
     while True:
 
-        player.movement()
+        clock.tick(60)
+
+        tiempo = pygame.time.get_ticks()/1000
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -54,13 +116,29 @@ def SpaceInvader():
             if inGame == True:
                 if event.type == pygame.KEYDOWN:
                     if event.key == K_LEFT:
-                        player.rect.left -= player.velocity
+                        player.leftMovement()
                     elif event.key == K_RIGHT:
-                        player.rect.right += player.velocity
+                        player.rightMovement()
                     elif event.key == K_SPACE:
-                         player.shoot()
+                         x,y = player.rect.center
+                         player.shoot(x,y)
         screen.blit(background,(0,0))
+        
+
+        enemy.behavior(tiempo)
+
         player.draw(screen)
+        enemy.draw(screen)
+
+        if len(player.shootList)>0:
+            for x in player.shootList:
+                x.draw(screen)
+                x.trayectory()
+
+                if x.rect.top < -10:
+                    player.shootList.remove(x)
+
+
         pygame.display.update()
 
 SpaceInvader()
